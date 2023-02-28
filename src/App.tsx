513b07d3a0965858ddc2e591f23d21d5ba9cc349
wellingtonf@alunos.utfpr.edu.br <wellingtonf@alunos.utfpr.edu.br>
2023-02-28 13:55:47 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import {
 function App() {
   
   const { unityProvider, isLoaded, loadingProgression, sendMessage } = useUnityContext({
-    loaderUrl: "Build/E-Battle Build(JSON-API).loader.js",
-    dataUrl: "Build/E-Battle Build(JSON-API).data.unityweb",
-    frameworkUrl: "Build/E-Battle Build(JSON-API).framework.js.unityweb",
-    codeUrl: "Build/E-Battle Build(JSON-API).wasm.unityweb",
+    loaderUrl: "Build2/E-Battle Build(JSON-API).loader.js",
+    dataUrl: "Build2/E-Battle Build(JSON-API).data.unityweb",
+    frameworkUrl: "Build2/E-Battle Build(JSON-API).framework.js.unityweb",
+    codeUrl: "Build2/E-Battle Build(JSON-API).wasm.unityweb",
     webglContextAttributes: { preserveDrawingBuffer: true },
   });
 
   const loadingPercentage = Math.round(loadingProgression * 100);
 
   function HandlerClick() {
-    sendMessage('Canvas', 'SetIdGame', "62");
+    sendMessage('Canvas', 'SetIdMatche', "37");
     sendMessage('Canvas', 'SetTokenAPI', "26c510da94f75a6eecdd9e30d695830d84de9a4928b4fa79a2ef0dc5c258d3ba115a1f5e5656268b58b190469b966fd9d837ce032f3d9edff8c7edf05ecd8472");
   }
 
